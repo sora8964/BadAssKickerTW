f1ea31c7c4e39bb28a5b326b46ab24fab30dc505
Nakade Hitsujiko <sora8964@gmail.com>
2024-03-16 00:05:25 +0800

    first commit

--- main.js
+++ main.js
@@ -111,7 +111,68 @@ app.whenReady().then(() => {
       if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
 
-    require("./menu.js")();
+    /* https://www.electronjs.org/docs/latest/api/menu */
+    const isMac = process.platform === 'darwin'
+    const menuTemplate =  [
+        // { role: 'appMenu' }
+        ...(isMac
+        ? [{
+            label: app.name,
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideOthers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+            }]
+        : []),
+        {
+        /*label: 'View',*/
+        label: "檔案",
+        submenu: [
+            { role: 'reload' },{ role: 'forceReload' },{ role: 'toggleDevTools' },
+            /*{ type: 'separator' },{ role: 'resetZoom' },{ role: 'zoomIn' },{ role: 'zoomOut' },*/
+            { type: 'separator' },{ role: 'togglefullscreen' }
+        ]
+        },
+        {
+        label: "設定",
+        submenu: [
+            {
+                label: '設置輸出目錄',
+                click: async () => {
+                    setOutputDirectory();
+                }
+            },
+            {
+                label: '設置FFMpeg位置',
+                click: async () => {
+                    setFFMpegDirectory();
+                }
+            }
+        ]
+        },
+        {
+        /*role: 'help',*/
+        label: "幫助",
+        submenu: [
+            {
+            label: '檢舉神器 on GitHub',
+            click: async () => {
+                const { shell } = require('electron')
+                await shell.openExternal('https://github.com/sora8964/badass-kicker-tw')
+            }
+            }
+        ]
+        },
+    ];
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
 })
 
 app.on('window-all-closed', () => {
